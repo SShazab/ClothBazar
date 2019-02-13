942519930b8eb3a5db3956fed0714080b17cabda
shazi <shazi@DESKTOP-NKAU9NC>
2019-02-14 00:45:18 +0500

    CRUD complete Theme apply success

---
+++ ClothBazar.Services/CategoriesService.cs
@@ -0,0 +1,60 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class CategoriesService
+    {
+        public Category GetCategory(int ID)
+        {
+
+            using (var context = new CBContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+        public List<Category>GetCategories()
+        {
+            
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        public void SaveCategory(Category category)
+        {
+            // object
+            using (var context = new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateCategory(Category category)
+        {
+            // object
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteCategory(int ID)
+        {
+            // object
+            using (var context = new CBContext())
+            {
+                var category = context.Categories.Find(ID);
+                context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                //both working same above or below
+                //context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothBazar.Services/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class ProductsService
+    {
+        public Product GetProduct(int ID)
+        {
+
+            using (var context = new CBContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+        public List<Product>GetProducts()
+        {
+            
+            using (var context = new CBContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public void SaveProduct(Product product)
+        {
+            // object
+            using (var context = new CBContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            // object
+            using (var context = new CBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int ID)
+        {
+            // object
+            using (var context = new CBContext())
+            {
+                var product = context.Products.Find(ID);
+                context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                //both working same above or below
+                //context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
